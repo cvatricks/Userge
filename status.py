15d0e63d0e564e4d23ed10af5b1deeaeb2859edc
cvatricks <68840422+cvatricks@users.noreply.github.com>
2022-04-27 20:16:15 +0530

    Update status.py

--- status.py
+++ status.py
@@ -3,7 +3,7 @@ import time
 import datetime
 
 import pyrogram
-import Config
+import config
 
 #user_session_string = os.environ.get("user_session_string")
 bots = [i.strip() for i in os.environ.get("BOTS").split(' ')]
