ed8cacb059097f44f88ffd925fca3707e999e582
cvatricks <68840422+cvatricks@users.noreply.github.com>
2022-04-27 20:48:05 +0530

    Update status.py

--- status.py
+++ status.py
@@ -3,7 +3,9 @@ import time
 import datetime
 
 import pyrogram
+from userge import userge, config
 
+@userge.on_start
 #user_session_string = os.environ.get("user_session_string")
 bots = [i.strip() for i in os.environ.get("BOTS").split(' ')]
 bot_owner = os.environ.get("OWNER_ID")
